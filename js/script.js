090404c765bc1de18c57a9e8dc286b6e0388e6ae
kalashnikovsb <kalashnikov.sk8@gmail.com>
2022-02-01 00:58:48 +0700

    Верстка главной

--- js/script.js
+++ js/script.js
@@ -4,13 +4,15 @@ const productSlides = document.querySelectorAll('.slider-products__item');
 const buttonPrev = document.querySelector('.slider-products__button--prev');
 const buttonNext = document.querySelector('.slider-products__button--next');
 const radioControls = document.querySelectorAll('.slider-products__control-input');
+const servicesControls = document.querySelectorAll('.slider-services__control');
+const servicesSlides = document.querySelectorAll('.slider-services__slide');
 
 
 const showCurrentSlide = () => {
   for (let slide of productSlides) {
-    slide.classList.remove(className);
+    slide.classList.remove(productClassName);
   }
-  productSlides[currentSlide].classList.add(className);
+  productSlides[currentSlide].classList.add(productClassName);
 };
 
 
@@ -19,6 +21,20 @@ const refreshControls = () => {
     control.checked = false;
   }
   radioControls[currentSlide].checked = true;
+};
+
+const resetSlider = () => {
+  for (let control of servicesControls) {
+    control.classList.remove(activeControlClassName);
+  }
+  for (let slide of servicesSlides) {
+    slide.classList.remove(activeSlideClassName);
+  }
+};
+
+const setClasses = (i) => {
+  servicesControls[i].classList.add(activeControlClassName);
+  servicesSlides[i].classList.add(activeSlideClassName);
 };
 
 
@@ -47,7 +63,21 @@ radioControls.forEach((control, i) => {
   });
 });
 
-const className = 'slider-products__item--active';
+const productClassName = 'slider-products__item--active';
+const activeControlClassName = 'slider-services__control--active';
+const activeSlideClassName = 'slider-services__slide--active';
 let currentSlide = 0;
 
-productSlides[0].classList.add(className);
+productSlides[0].classList.add(productClassName);
+servicesControls[0].classList.add(activeControlClassName);
+servicesSlides[0].classList.add(activeSlideClassName);
+
+servicesControls.forEach((control, i) => {
+  control.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    resetSlider();
+    setClasses(i);
+  });
+});
+
+
