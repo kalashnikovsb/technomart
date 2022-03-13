7f9119422d0f6d0e777a184288515da76d782440
kalashnikovsb <kalashnikov.sk8@gmail.com>
2022-03-13 20:07:10 +0700

    Оживление интерфейса

--- js/script.js
+++ js/script.js
@@ -81,3 +81,98 @@ servicesControls.forEach((control, i) => {
 });
 
 
+
+
+const mapPopup = document.querySelector('.map-popup');
+const openMapElement = document.querySelector('.contacts__map-image');
+const closeMapElement = document.querySelector('.map-popup__close');
+
+const mapPopupOpen = (evt) => {
+  evt.preventDefault();
+  mapPopup.classList.add('map-popup--show');
+};
+
+const mapPopupClose = (evt) => {
+  evt.preventDefault();
+  mapPopup.classList.remove('map-popup--show');
+};
+
+openMapElement.addEventListener('click', mapPopupOpen);
+closeMapElement.addEventListener('click', mapPopupClose);
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains('map-popup--show') || feedbackPopup.classList.contains('feedback-popup--show')) {
+      mapPopupClose(evt);
+      feedbackPopupClose(evt);
+    }
+  }
+});
+
+
+
+
+
+const feedbackPopup = document.querySelector('.feedback-popup');
+const feedbackForm = document.querySelector('.feedback-popup__form');
+const feedbackOpenElement = document.querySelector('.button--contacts');
+const feedbackCloseElement = document.querySelector('.feedback-popup__close');
+const feedbackLogin = document.querySelector('#customer-name');
+const feedbackEmail = document.querySelector('#customer-email');
+
+let isStorageSupport = true;
+let storage = '';
+
+try {
+  storage = localStorage.getItem('login');
+} catch (err) {
+  isStorageSupport = false;
+}
+
+const feedbackPopupOpen = (evt) => {
+  evt.preventDefault();
+  feedbackPopup.classList.add('feedback-popup--show');
+
+  if (storage) {
+    feedbackLogin.value = storage;
+    feedbackEmail.focus();
+  } else {
+    feedbackLogin.focus();
+  }
+};
+
+const feedbackPopupClose = (evt) => {
+  evt.preventDefault();
+  feedbackPopup.classList.remove('feedback-popup--show');
+  feedbackPopup.classList.remove('feedback-popup--error');
+};
+
+feedbackOpenElement.addEventListener('click', feedbackPopupOpen);
+feedbackCloseElement.addEventListener('click', feedbackPopupClose);
+
+feedbackForm.addEventListener('submit', (evt) => {
+  if (!feedbackLogin.value || !feedbackEmail.value) {
+    evt.preventDefault();
+    feedbackPopup.classList.remove('feedback-popup--error');
+    feedbackForm.offsetWidth == feedbackForm.offsetWidth;
+    feedbackPopup.classList.add('feedback-popup--error');
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem('login', feedbackLogin.value);
+    }
+  }
+});
+
+
+
+
+const cartPopup = document.querySelector('.cart-popup');
+const cartLink = document.querySelector('.checkout-block__link--cart');
+const buyButtons = document.querySelectorAll('.button--buy');
+
+buyButtons.forEach((button) => {
+  button.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    cartLink.classList.add('.checkout-block__link--active');
+  });
+});
